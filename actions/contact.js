3a69793e6eb924756ff1be8c7a111df3f1de5a73
Ihtisham Ul Haq <developer@affotax.com>
2024-06-27 12:39:56 +0500

    contact us page update

--- actions/contact.js
+++ actions/contact.js
@@ -1,27 +1,19 @@
-'use server';
+"use server";
 
 import { getFormData } from "@/lib/getFormData";
 import sendMail from "@/lib/sendMail";
 
-
-
-
-
-
-
-
-
-
 //  SEND MESSAGE
 
-export async function sendMessage(formData) {
-        
+export async function sendMessage(prevState, formData) {
+	const { name, email, service, message } = getFormData( formData, "name", "email", "service", "message" );
 
-        //const {name, email, service, message} = getFormData(formData, 'name', 'email', 'service', 'message');
-
-        //console.log(email, name)
-
-        const res = await sendMail();
-
-        console.log(res)
+	try {
+		const res = await sendMail(name, email, service, message);
+		if (res) {
+                        return { success: true, message: `Your query is submitted | We'll get back to you soon`, };
+                }
+	} catch (error) {
+		return { success: false, message: "Error occured while sending email | Please try again later", };
+	}
 }
--- components/Contact Us/Form.jsx
+++ components/Contact Us/Form.jsx
@@ -2,12 +2,15 @@
 
 import { sendMessage } from '@/actions/contact';
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Instagram from '@/public/ig.png';
 import Facebook from '@/public/fb.png';
 import LinkedIn from '@/public/li.png';
 import Whatsapp from '@/public/wa.png';
 import Image from 'next/image';
+import {  useFormState } from "react-dom";
+import FormBtn from './FormBtn';
+
 
 const Form = () => {
 
@@ -16,9 +19,9 @@ const Form = () => {
         const [service, setService] = useState('');
         const [message, setMessage] = useState('');
         const [error, setError] = useState('');
-        const [successMessage, setSuccessMessages] = useState('')
-        const [isLoading, setIsLoading] = useState(false);
-
+        
+        const [state, formAction] = useFormState(sendMessage, {success:false, message: ''});
+        const formRef = useRef()
 
         const inputChangeHandler = (e) => {
             switch(e.target.name) {
@@ -32,52 +35,29 @@ const Form = () => {
 
 
         const formActionHandler = async (event) => {
-            let error = false;
-            
-            setError('');
-            
+
             if(name.length === 0 || email.length === 0 || message.length === 0) {
-                error = true;
-                setError('All the fields are required.')
+                setError('All the fields are required.');
+            } else {
+                setError('');
+                await formAction(event);
+
             }
-            
-            
-            
-            
-            
-            setIsLoading(true);
-
-            if(!error) {
-                
-                try {
-                    const res = await sendMessage(event);
-                    console.log('response' , res);
-                    
-                    setIsLoading(false);
-                    setSuccessMessages('Your query is submitted');
-
-                    setName('');
-                    setEmail('');
-                    setService('');
-                    setMessage('')
-
-
-                } catch (err) {
-                    setError('catch error block ')
-                    setIsLoading(false);
-                }
-
-                
-            }
-
-
             return;
 
-
-
-
-
         }
+
+
+        useEffect(() => {
+            if (state.success) {
+                setName('');
+                setEmail('');
+                setService('');
+                setMessage('');
+            }
+
+        })
+
 
 
   return (
@@ -85,24 +65,24 @@ const Form = () => {
         <div className='flex flex-col w-[40%] max-lg:w-full gap-4 max-lg:hidden'>
             <h2 className='text-primary text-6xl font-semibold '>Get In Touch</h2>
             <p>Fill out the form and a member of our team will be in touch soon.</p>
-            <Link href='https://wa.me/447723143223' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Whatsapp} /> Whatsapp </Link>
-            <Link href='https://www.instagram.com/affotax_/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Instagram} /> Instagram</Link>
-            <Link href='https://www.facebook.com/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Facebook} /> Facebook</Link>
-            <Link href='https://www.linkedin.com/company/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={LinkedIn} /> LinkedIn</Link>
+            <Link href='https://wa.me/447723143223' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Whatsapp} alt='whatsapp'/> Whatsapp </Link>
+            <Link href='https://www.instagram.com/affotax_/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Instagram} alt='instagram'/> Instagram</Link>
+            <Link href='https://www.facebook.com/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Facebook} alt='facebook'/> Facebook</Link>
+            <Link href='https://www.linkedin.com/company/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={LinkedIn} alt='linkedin'/> LinkedIn</Link>
         </div>
 
         <div className='hidden flex-col w-[40%] max-lg:w-full gap-4 max-lg:flex mb-4'>
             <h2 className='text-primary text-5xl font-semibold '>Get In Touch</h2>
             <p>Fill out the form and a member of our team will be in touch soon.</p>
             <div className='grid grid-cols-2 gap-3 '>
-            <Link href='https://wa.me/447723143223' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Whatsapp} /> Whatsapp </Link>
-            <Link href='https://www.instagram.com/affotax_/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Instagram} /> Instagram</Link>
-            <Link href='https://www.facebook.com/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Facebook} /> Facebook</Link>
-            <Link href='https://www.linkedin.com/company/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={LinkedIn} /> LinkedIn</Link>
+            <Link href='https://wa.me/447723143223' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Whatsapp} alt='whatsapp'/> Whatsapp </Link>
+            <Link href='https://www.instagram.com/affotax_/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Instagram} alt='instagram'/> Instagram</Link>
+            <Link href='https://www.facebook.com/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={Facebook} alt='facebook'/> Facebook</Link>
+            <Link href='https://www.linkedin.com/company/affotax/' className='flex flex-row w-10 items-center gap-4 font-semibold text-xl hover:text-primary '><Image src={LinkedIn} alt='linkedin'/> LinkedIn</Link>
             </div>
         </div>
         <div className=' bg-white w-[60%] max-lg:w-full px-10 py-10 drop-shadow-xl rounded-2xl '>
-            <form action={formActionHandler} className='flex flex-col  items-start gap-4'>
+            <form ref={formRef} action={formActionHandler}  className='flex flex-col  items-start gap-4'>
                 <input className='px-4 py-3 bg-white rounded-lg w-full border-2 border-black/40 shadow-md shadow-black/10  outline-none focus:border-primary focus:shadow-primary/30' type='text' name='name' placeholder='Name' value={name} onChange={inputChangeHandler}/>
                 <input className='px-4 py-3 bg-white rounded-lg w-full border-2 border-black/40 shadow-md shadow-black/10 outline-none focus:border-primary focus:shadow-primary/30' type='email' name='email' placeholder='Email' value={email} onChange={inputChangeHandler}/>
                 <select className='px-4 py-3 bg-white rounded-lg w-full border-2 border-black/40 shadow-md shadow-black/10  outline-none focus:border-primary focus:shadow-primary/30' name="service" value={service} onChange={inputChangeHandler}> 
@@ -116,9 +96,9 @@ const Form = () => {
                     <option value="Others">Others</option>
                  </select>
                 <textarea className='px-4 py-3 bg-white rounded-lg w-full border-2 border-black/40 shadow-md shadow-black/10 outline-none focus:border-primary focus:shadow-primary/30'  name="message" placeholder='Write your message' value={message} onChange={inputChangeHandler}></textarea>
+                {state.message && <p className={`${state.success ? 'text-green-600' : 'text-red-600'} text-sm`}>{state.message}</p>}
                 {error && <p className='text-red-600 text-sm '>{error}</p>}
-                {successMessage && <p className='text-green-600 text-sm '>{error}</p>}
-                <button className='drop-shadow-md transition-all inline-flex text-secondary bg-primary border-0 py-2 px-6 hover:outline-primary outline-1 outline outline-transparent hover:bg-transparent hover:text-primary rounded-md text-md font-semibold  active:scale-95  ' > Send Message </button>
+                <FormBtn />
             </form>
         </div>
     </div>
