3e010cfcaa05c8ab26c2a2b57c279136ce38e04f
Ihtisham Ul Haq <developer@affotax.com>
2024-06-28 19:21:44 +0500

    add pricing bottom line

--- components/Home/Subpages/ChoosePackage.jsx
+++ components/Home/Subpages/ChoosePackage.jsx
@@ -212,7 +212,7 @@ export default function ChoosePackage() {
                     </svg>
                     </button></Link>
                     <p className="text-xs text-gray-500 mt-3">
-                    Making tax filing affordable with 100% satisfaction and 30-day guarantee.
+                    Making tax filing affordable with 100% satisfaction and 30-day Money-Back guarantee.
                     </p>
                 </div>
                 </div>
@@ -338,7 +338,7 @@ export default function ChoosePackage() {
                     </svg>
                     </button></Link>
                     <p className="text-xs text-gray-500 mt-3">
-                    Making tax filing affordable with 100% satisfaction and 30-day guarantee.
+                    Making tax filing affordable with 100% satisfaction and 30-day Money-Back guarantee.
                     </p>
                 </div>
                 </div>
@@ -467,7 +467,7 @@ export default function ChoosePackage() {
                     </button>
                     </Link>
                     <p className="text-xs text-gray-500 mt-3">
-                    Making tax filing affordable with 100% satisfaction and 30-day guarantee.
+                    Making tax filing affordable with 100% satisfaction and 30-day Money-Back guarantee.
                     </p>
                 </div>
                 </div>
