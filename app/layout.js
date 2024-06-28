2ca5706618f1d074a4eb1803d1109dfdf3c38ae8
Ihtisham Ul Haq <developer@affotax.com>
2024-06-28 12:17:09 +0500

    tawk add

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,19 @@
+'use client'
+
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import "./globals.css";
 import { IoLogoWhatsapp } from "react-icons/io";
 import Link from "next/link";
+import Script from "next/script";
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 
 
-export const metadata = {
-  title: "The Affotax Accountants",
-  description: "The Affotax Accountants | Making Tax Affordable",
-};
+// export const metadata = {
+//   title: "The Affotax Accountants",
+//   description: "The Affotax Accountants | Making Tax Affordable",
+// };
 
 export default function RootLayout({ children }) {
   return (
@@ -22,8 +26,43 @@ export default function RootLayout({ children }) {
         {children}
         </div>
         <Footer />
+
+        
+        <TawkMessengerReact
+                propertyId="667e5ad19d7f358570d4466f"
+                widgetId="1i1ep5goa"/>
         </body>
         
     </html>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+              var s1=document.createElement("script");
+              var s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/667e5ad19d7f358570d4466f/1i1ep5goa';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+        })();
+        </script> */}
