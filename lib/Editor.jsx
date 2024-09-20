45b5b092094bdf931641a3fbe202e69c0caada65
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2024-09-20 12:23:25 +0500

    sitemap update whowehelp

--- lib/Editor.jsx
+++ lib/Editor.jsx
@@ -3,13 +3,54 @@
 import "react-quill/dist/quill.snow.css";
 import ReactQuill, { Quill } from "react-quill";
 import QuillResizeImage from "quill-resize-image";
+import QuillBetterTable from 'quill-better-table'
+import { useEffect, useRef } from "react";
 
 Quill.register("modules/resize", QuillResizeImage);
+// Quill.register({
+// 	'modules/better-table': QuillBetterTable
+//   }, true)
 
 export function CustomEditor({ value, setValue }) {
 
 
+
+	const quillRef = useRef()
+
+
+
+	// useEffect(() => {
+	// 	console.log(quillRef.current)
+
+	// 	const quill = quillRef.current.getEditor();
+	// 	const table = QuillBetterTable.createTable(3, 3); // Example: create a 3x3 table
+	// 	quill.insertEmbed(quill.getSelection().index, 'table', table);
+
+
+	// }, [])
+
+
 	const modules = {
+
+
+	// 	table: false,  // disable table module
+    // 'better-table': {
+    //   operationMenu: {
+    //     items: {
+    //       unmergeCells: {
+    //         text: 'Another unmerge cells name'
+    //       }
+    //     },
+    //     color: {
+    //       colors: ['#fff', 'red', 'rgb(0, 0, 0)'],  // colors in operationMenu
+    //       text: 'Background Colors'  // subtitle
+    //     } 
+    //   }
+    // },
+    // keyboard: {
+    //   bindings: QuillBetterTable.keyboardBindings
+    // },
+
 
 		
 
@@ -43,6 +84,6 @@ export function CustomEditor({ value, setValue }) {
 	};
 
 	return (
-		<ReactQuill className="w-full" theme="snow" value={value} onChange={setValue} modules={modules} />
+		<ReactQuill ref={quillRef} className="w-full" theme="snow" value={value} onChange={setValue} modules={modules} />
 	);
 }
