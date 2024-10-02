a76839ac82d6263a2588af79670f9cdb1647b3c1
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2024-10-02 16:37:24 +0500

    leftSection update success

--- components/Services/Service/LeftSection.jsx
+++ components/Services/Service/LeftSection.jsx
@@ -12,7 +12,7 @@ const LeftSection = ({ title, content }) => {
 
 
     return (
-        <section className="w-full font-poppins customCss bg-slate-50 rounded-xl p-12  ">
+        <section className="w-full  font-poppins customCss bg-slate-50 rounded-xl p-12  ">
             <div className="container mx-auto flex flex-col justify-center items-start  ">
                 {/* <h1 className="text-3xl font-semibold ">{title}</h1> */}
 
--- components/Services/Service/TopSection.jsx
+++ components/Services/Service/TopSection.jsx
@@ -19,7 +19,7 @@ const TopSection = ({ title, content }) => {
         
             <div className="   flex  justify-center items-center gap-4  px-8 bg-white      py-2 rounded-full   transform  shadow-[5px_5px_0px_0px_rgba(255,185,123)] border-2 border-orange-300 ">
             <PiArrowFatLineRightFill className="text-5xl text-orange-400 max-lg:hidden "/>
-                <h1 className=" py-4 leading-relaxed drop-shadow-md   text-center bg-gradient-to-br from-orange-400 via-orange-500 via-50% to-orange-700 bg-clip-text   text-2xl max-lg:py-0   font-semibold text-transparent font-poppins  lg:max-w-3xl lg:text-6xl">{title}</h1>
+                <h1 className=" py-4 leading-relaxed drop-shadow-md   text-center bg-gradient-to-br from-orange-400 via-orange-500 via-50% to-orange-700 bg-clip-text   text-2xl max-lg:py-0   font-semibold text-transparent font-poppins  lg:max-w-3xl lg:text-4xl">{title}</h1>
                 
                
 
