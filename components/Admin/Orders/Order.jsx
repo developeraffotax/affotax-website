afec592164c4da8abdf3f786510d9b775c31ccce
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-03-05 13:12:47 +0500

    comments remove admin

--- components/Admin/Orders/Order.jsx
+++ components/Admin/Orders/Order.jsx
@@ -1,331 +1,193 @@
-'use client'
-
-
-import { deleteBlog } from '@/lib/blogs';
-import { Badge, ConfigProvider, Input, List, Popconfirm, Space, Spin, Table, Tag, Typography } from 'antd';
-import axios from 'axios';
-import Link from 'next/link';
-import { MdDelete } from "react-icons/md";
-import { useEffect, useRef, useState } from 'react';
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { BiEdit } from "react-icons/bi";
-import { AiOutlineEye } from "react-icons/ai";
-import { useRouter } from 'next/navigation';
-import { FcSearch } from 'react-icons/fc';
-import dayjs from 'dayjs';
-
-
-
-
-
-
-
-
-export default function Order({orderData}) {
-
-    console.log(orderData)
-
-    const router = useRouter()
-
-    const tableRef = useRef()
-    
-    const [isLoading, setIsLoading] = useState(false);
-
-     const [order, setOrder] = useState(JSON.parse(orderData));
-     
-     const [orderItems, setOrderItems] = useState([]);
-    
-    // const isEditing = (record) => record.key === editingKey;
-
-    
-
-    // const [orderedItems, setOrderedItems] = useState(JSON.parse(orderData));
-    // const [addOns, setAddOns] = useState()
-       
-
-  
-
-
-
-
-
-    useEffect(() => {
-
-        console.log(JSON.parse(orderData))
-
-        const newOrderData = JSON.parse(orderData)
-        setOrderItems((prev) => {
-
-
-             return newOrderData.items.map((el) => {
-                return {
-                    ...el,
-                     key: el._id
-                }
-            })
-
-        })
-
-
-    }, [])
-
-
-    // const expandColumns = [
-         
-    //     {
-    //       title: 'Title',
-    //       dataIndex: 'priceTitle',
-    //       key: 'priceTitle',
-          
-    //     },
-    //     {
-    //       title: 'Price',
-    //       key: 'price',
-    //       render: (text) => <p className=" font-semibold   ">£{text}</p>,
-    //     },
-    // ]
-
-
-    // const expandedRowRender = () => (
-    //     <Table columns={expandColumns} dataSource={expandDataSource} pagination={false} />
-    //   ); 
-    
-
-
-
-
-
-
-
-    const columns = [
-        // {
-        //     title: 'Sr.',
-        //     dataIndex: 'key',
-        //     width: '6%',
-        //     key: 'key',
-        //     render: (text, record, index) => <strong className="flex font-poppins items-center justify-center h-full w-full bg-orange-100 text-orange-800 font-semibold  rounded-xl p-2" >{text}</strong>,
-            
-        //   },
-
-        {
-          title: 'Item Name',
-        //   dataIndex: 'orderNumber',
-          key: 'item_name',
-          render: (text, record) => <p className='font-semibold '>{`${record.priceTitle} | ${record.pageTitle}`}</p>,
-          
-          
-        },
-        
-        
-
-          {
-            title: 'Price',
-            dataIndex: 'price',
-            key: 'price',
-            render: (text) => <p className=" font-semibold   ">£{text}</p>,
-            
-          },
-
-        //   {
-        //     title: 'VAT | 20%',
-        //     dataIndex: 'vat',
-        //     key: 'vat',
-        //     render: (text) => <p className="   ">£{text}</p>,
-            
-        //   },
-
-
-         
-
-          
-      ];
-
-
-
- 
-
-
-      const expandedRowRender = (record, index, indent, expanded) => {
-
-            
-
-        console.log("THE RECORD IS >>>>>>>>>>>>>>>", record)
-        return   <List
-        // key={index}
-        itemLayout="horizontal"
-        dataSource={record.addOns}
-        header={<h3 className='font-semibold text-base w-full text-start bg-orange-100 text-orange-800  p-1 px-4   '>AddOns for {record.pageTitle}</h3>}
-        renderItem={(item, index) => (
-          <List.Item className='p-0 '>
-               
-
-                <table className=''  >
-                    {/* <thead>
-                        <tr >
-                            <td colSpan={2}>
-                                    <h3 className='font-poppins font-semibold text-base w-full text-center bg-orange-100 text-orange-800  p-2   '>AddOns for {record.pageTitle}</h3>
-                                </td> </tr>
-                    </thead> */}
-
-                        <tbody >
-
-                        <tr className='w-full grid grid-cols-2 justify-start gap-4 items-start  text-xs font-poppins    '>
-                            <td className=' font-[500]   text-start lg:px-4   '>{index + 1}. {item.priceTitle}</td>
-                            <td className=' font-[500]   text-end lg:px-4 '>£{item.price}</td>
-                        </tr>
-
-
-                        </tbody>
-                    
-                </table>
-          </List.Item>
-        )}
-      />
-      }
-
-
-
-
-    return (
-        <>
-
-
-
-<div className='w-full flex justify-start items-start gap-5 '>
-
-
-    
-<table className='border-collapse border w-full max-w-lg mb-4 '   >
-            <thead>
-                <tr>
-                    <td colSpan={2}>
-                        <h3 className='font-semibold text-lg w-full text-start bg-orange-100 text-orange-800  p-1 px-4 '>Order Details</h3>
-                    </td>
-                </tr>
-            </thead>
-
-
-            <tbody >
-
-                <tr className=' font-poppins border-b  font-[500] '>
-                    <td className='px-2' >Order Number</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>{order.orderNumber}</td>
-                </tr>
-
-                 <tr className=' font-poppins border-b  font-[500]  '>
-                    <td className='px-2'>Payment Status</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>{order.payment_status}</td>
-                </tr>
-
-                 <tr className=' font-poppins border-b  font-[500]  '>
-                    <td className='px-2'>Total Price without VAT</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>£{order.totalPriceWithoutVat}</td>
-                </tr>
-
-                 <tr className=' font-poppins border-b  font-[500]  '>
-                    <td className='px-2'>VAT | 20%</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>£{order.vat}</td>
-                </tr>
-
-                 <tr className=' font-poppins border-b  font-[500]  '>
-                    <td className='px-2'>Total Price</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>£{order.totalPrice}</td>
-                </tr>
-
-
-
-
-            </tbody>
-
-
-        </table>
-
-
-
-        
-        <table className='border-collapse border w-full max-w-lg mb-4 '   >
-            <thead>
-                <tr>
-                    <td colSpan={2}>
-                        <h3 className='font-semibold text-lg w-full text-start bg-orange-100 text-orange-800  p-1 px-4 '>Customer Details</h3>
-                    </td>
-                </tr>
-            </thead>
-
-
-            <tbody >
-
-                <tr className=' font-poppins border-b  font-[500] '>
-                    <td className='px-2' >Customer Name</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>{order.customer_name}</td>
-                </tr>
-
-                 <tr className=' font-poppins border-b  font-[500]  '>
-                    <td className='px-2'>Customer Email</td>
-                    <td className='bg-gray-100 px-4 py-2 w-[50%]  '>{order.customer_email}</td>
-                </tr>
-
-                 
-
-
-
-            </tbody>
-
-
-        </table>
-
-
-</div>
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <Input className=" mb-6 "  style={{ width: '30%', }} prefix={<FcSearch className="text-lg " />}  placeholder="Enter the Page Title OR URL" onChange={onChange}    allowClear /> */}
-<h2 className='font-poppins font-semibold text-base w-full text-center bg-orange-100 text-black  p-2   '>Ordered Items</h2>
-<Table  className=''  columns={columns} expandable={{
-        // expandedRowKeys: ['0'],
-        expandedRowRender: expandedRowRender,
-        //   defaultExpandedRowKeys: ['0'],
-      }}
-      
-       dataSource={orderItems} size='large' showHeader bordered  />
-
-
- 
-        
-    
-        </>
-    )
+"use client";
+
+
+import { List, Table, } from "antd";
+import { useEffect,  useState } from "react";
+
+
+export default function Order({ orderData }) {
+
+
+	const [order, setOrder] = useState(JSON.parse(orderData));
+
+	const [orderItems, setOrderItems] = useState([]);
+
+
+
+	useEffect(() => {
+		console.log(JSON.parse(orderData));
+
+		const newOrderData = JSON.parse(orderData);
+		setOrderItems((prev) => {
+			return newOrderData.items.map((el) => {
+				return {
+					...el,
+					key: el._id,
+				};
+			});
+		});
+	}, []);
+
+
+
+	const columns = [
+		{
+			title: "Item Name",
+			//   dataIndex: 'orderNumber',
+			key: "item_name",
+			render: (text, record) => (
+				<p className="font-semibold ">{`${record.priceTitle} | ${record.pageTitle}`}</p>
+			),
+		},
+
+		{
+			title: "Price",
+			dataIndex: "price",
+			key: "price",
+			render: (text) => <p className=" font-semibold   ">£{text}</p>,
+		},
+	];
+
+
+
+	const expandedRowRender = (record, index, indent, expanded) => {
+		console.log("THE RECORD IS >>>>>>>>>>>>>>>", record);
+		return (
+			<List
+				// key={index}
+				itemLayout="horizontal"
+				dataSource={record.addOns}
+				header={
+					<h3 className="font-semibold text-base w-full text-start bg-orange-100 text-orange-800  p-1 px-4   ">
+						AddOns for {record.pageTitle}
+					</h3>
+				}
+				renderItem={(item, index) => (
+					<List.Item className="p-0 ">
+						<table className="">
+							<tbody>
+								<tr className="w-full grid grid-cols-2 justify-start gap-4 items-start  text-xs font-poppins    ">
+									<td className=" font-[500]   text-start lg:px-4   ">
+										{index + 1}. {item.priceTitle}
+									</td>
+									<td className=" font-[500]   text-end lg:px-4 ">
+										£{item.price}
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</List.Item>
+				)}
+			/>
+		);
+	};
+
+
+
+	return (
+		<>
+			<div className="w-full flex justify-start items-start gap-5 ">
+				<table className="border-collapse border w-full max-w-lg mb-4 ">
+					<thead>
+						<tr>
+							<td colSpan={2}>
+								<h3 className="font-semibold text-lg w-full text-start bg-orange-100 text-orange-800  p-1 px-4 ">
+									Order Details
+								</h3>
+							</td>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr className=" font-poppins border-b  font-[500] ">
+							<td className="px-2">Order Number</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								{order.orderNumber}
+							</td>
+						</tr>
+
+						<tr className=" font-poppins border-b  font-[500]  ">
+							<td className="px-2">Payment Status</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								{order.payment_status}
+							</td>
+						</tr>
+
+						<tr className=" font-poppins border-b  font-[500]  ">
+							<td className="px-2">Total Price without VAT</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								£{order.totalPriceWithoutVat}
+							</td>
+						</tr>
+
+						<tr className=" font-poppins border-b  font-[500]  ">
+							<td className="px-2">VAT | 20%</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								£{order.vat}
+							</td>
+						</tr>
+
+						<tr className=" font-poppins border-b  font-[500]  ">
+							<td className="px-2">Total Price</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								£{order.totalPrice}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+
+
+
+				<table className="border-collapse border w-full max-w-lg mb-4 ">
+					<thead>
+						<tr>
+							<td colSpan={2}>
+								<h3 className="font-semibold text-lg w-full text-start bg-orange-100 text-orange-800  p-1 px-4 ">
+									Customer Details
+								</h3>
+							</td>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr className=" font-poppins border-b  font-[500] ">
+							<td className="px-2">Customer Name</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								{order.customer_name}
+							</td>
+						</tr>
+
+						<tr className=" font-poppins border-b  font-[500]  ">
+							<td className="px-2">Customer Email</td>
+							<td className="bg-gray-100 px-4 py-2 w-[50%]  ">
+								{order.customer_email}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+
+
+			<h2 className="font-poppins font-semibold text-base w-full text-center bg-orange-100 text-black  p-2   ">
+				Ordered Items
+			</h2>
+
+			<Table
+				className=""
+				columns={columns}
+				expandable={{
+					 
+					expandedRowRender: expandedRowRender,
+					//   defaultExpandedRowKeys: ['0'],
+				}}
+				dataSource={orderItems}
+				size="large"
+				showHeader
+				bordered
+			/>
+		</>
+	);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//onClick={() => setData(prev => prev.filter(el => el._id !== record._id))}
+ 
--- components/Admin/Orders/Orders.jsx
+++ components/Admin/Orders/Orders.jsx
@@ -1,220 +1,64 @@
-'use client'
+"use client";
 
-
-import { deleteBlog } from '@/lib/blogs';
-import { ConfigProvider, Input, Space, Spin, Table, Tag } from 'antd';
-import axios from 'axios';
-import Link from 'next/link';
-import { MdDelete } from "react-icons/md";
-import { useEffect, useRef, useState } from 'react';
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { BiEdit } from "react-icons/bi";
-import { AiOutlineEye } from "react-icons/ai";
-import { useRouter } from 'next/navigation';
-import { FcSearch } from 'react-icons/fc';
-import dayjs from 'dayjs';
+import { Table } from "antd";
+import Link from "next/link";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 
 
+export default function Orders({ ordersData }) {
+
+	const [orders, setOrders] = useState(JSON.parse(ordersData));
 
 
+	const columns = [
+		{
+			title: "Order Number",
+			dataIndex: "orderNumber",
+			key: "orderNumber",
+			render: (text, record) => ( <Link href={`/admin/orders/${record._id}`}>{text}</Link> ),
+		},
+		{
+			title: "Payment Status",
+			dataIndex: "payment_status",
+			key: "payment_status",
+		},
 
-export default function Orders({ordersData}) {
+		{
+			title: "Total Price",
+			dataIndex: "totalPrice",
+			key: "totalPrice",
+			render: (text) => ( <p className="  font-semibold text-base   text-black px-2 py-1 rounded-md  "> £{text} </p> ),
+		},
 
-    const router = useRouter()
+		{
+			title: "Total Price Without VAT",
+			dataIndex: "totalPriceWithoutVat",
+			key: "totalPriceWithoutVat",
+			render: (text) => <p className=" ">£{text}</p>,
+		},
 
-    const tableRef = useRef()
-    
-    const [isLoading, setIsLoading] = useState(false);
+		{
+			title: "VAT | 20%",
+			dataIndex: "vat",
+			key: "vat",
+			render: (text) => <p className=" ">£{text}</p>,
+		},
 
-    const [orders, setOrders] = useState(JSON.parse(ordersData));
-    
-
-
-       
-    
-        // const [fetchedData, setFetchedData] = useState([]);
-        // const [filteredData, setFilteredData] = useState([]);
-
-       
+		{
+			title: "Ordered At",
+			dataIndex: "createdAt",
+			key: "createdAt",
+			render: (text) => ( <p className="font-poppins  bg-orange-100 text-orange-800 px-2 py-1 rounded-md  "> {dayjs(text).format("DD-MMM-YYYY  hh:mm A")} </p> ),
+		},
+	];
 
   
 
-
-
-
-
-
-    //   //Search Filter
-    //   const onChange = (event) => {
-        
-    //     const value = event.target.value;
-
-    //     const filteredArr = fetchedData.filter((el) => {
-
-    //         const typedValue = value.trim().toLowerCase();
-    //         const titleValue = el.title.trim().toLowerCase();
-    //         const isPresent = titleValue.includes(typedValue);
-            
-    //         if(!isPresent) {
-
-    //             let typed = typedValue || '';
-    //             if (typed.charAt(0) === '/') {   
-    //                 typed = typed.slice(1);
-    //             }
-                
-    //             const isUrlSame = el.slug?.includes(typed);
-    //             return isUrlSame;
-
-    //         }
-
-    //         return isPresent
-    //     });
-
-
-    //     setFilteredData(filteredArr);
-
-    // }
-
-
-
-    useEffect(() => {
-
-        
-        console.log(JSON.parse(ordersData))
-
-
-
-    }, [])
-
-
-    
-
-
-
-
-
-
-
-
-
-
-    const columns = [
-        // {
-        //     title: 'Sr.',
-        //     dataIndex: 'key',
-        //     width: '6%',
-        //     key: 'key',
-        //     render: (text, record, index) => <strong className="flex font-poppins items-center justify-center h-full w-full bg-orange-100 text-orange-800 font-semibold  rounded-xl p-2" >{text}</strong>,
-            
-        //   },
-        {
-          title: 'Order Number',
-          dataIndex: 'orderNumber',
-          key: 'orderNumber',
-          render: (text, record) => <Link href={`/admin/orders/${record._id}`}>{text}</Link>,
-          
-        },
-        {
-          title: 'Payment Status',
-          dataIndex: 'payment_status',
-          key: 'payment_status',
-          
-        },
-
-        {
-            title: 'Total Price',
-            dataIndex: 'totalPrice',
-            key: 'totalPrice',
-            render: (text) => <p className="  font-semibold text-base   text-black px-2 py-1 rounded-md  ">£{text}</p>,
-            
-          },
-
-          {
-            title: 'Total Price Without VAT',
-            dataIndex: 'totalPriceWithoutVat',
-            key: 'totalPriceWithoutVat',
-            render: (text) => <p className="   ">£{text}</p>,
-            
-          },
-
-          {
-            title: 'VAT | 20%',
-            dataIndex: 'vat',
-            key: 'vat',
-            render: (text) => <p className="   ">£{text}</p>,
-            
-          },
-
-
-          {
-            title: 'Ordered At',
-            dataIndex: 'createdAt',
-            key: 'createdAt',
-            render: (text) => <p className="font-poppins  bg-orange-100 text-orange-800 px-2 py-1 rounded-md  ">{dayjs(text).format("DD-MMM-YYYY  hh:mm A")}</p>,
-            
-          },
-
-        // {
-        //   title: 'Slug',
-        //   dataIndex: 'slug',
-        //   key: 'slug',
-          
-        //   render: (text) => <p className="font-poppins  bg-orange-100 text-orange-800 px-2 py-1 rounded-md  ">/{text}</p>,
-          
-        // },
-        
-        
-        // {
-        //   title: 'Action',
-        //   key: 'action',
-        //   render: (_, record) => {
-        //         console.log(record)
-        //     return <Space size="middle"> <Link href={`/blog/${record.slug}`} target='_blank' ><AiOutlineEye className='text-sky-500 scale-150 active:scale-125 hover:scale-[1.7] transition-all' /></Link> <Link href={`/admin/view-blogs/edit/${record._id}`}><BiEdit className='text-green-500 scale-150 active:scale-125 hover:scale-[1.7] transition-all'/></Link> <Link href={`/admin/view-blogs/delete/${record._id}`}><RiDeleteBin6Line className='text-red-500 scale-150 active:scale-125 hover:scale-[1.7] transition-all' /></Link> </Space>
-        //   },
-        // },
-      ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return (
-        <>
-
-{/* <Input className=" mb-6 "  style={{ width: '30%', }} prefix={<FcSearch className="text-lg " />}  placeholder="Enter the Page Title OR URL" onChange={onChange}    allowClear /> */}
-
-<Table columns={columns} dataSource={orders} size='large' showHeader bordered  />
-
-
- 
-        
-    
-        </>
-    )
+	return (
+		<>
+			<Table columns={columns} dataSource={orders} size="large" showHeader bordered />
+		</>
+	);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//onClick={() => setData(prev => prev.filter(el => el._id !== record._id))}
