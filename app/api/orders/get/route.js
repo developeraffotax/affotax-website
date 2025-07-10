6129d02280cb25a4ba9ca9ba41356b0a3b5a5ce7
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2025-07-10 16:27:15 +0500

    filter add in orders

--- app/api/orders/get/route.js
+++ app/api/orders/get/route.js
@@ -7,8 +7,23 @@ export const dynamic = "force-dynamic";
 export async function GET(req) {
     try {
         const db = await connectDB();
+
+        const { searchParams } = new URL(req.url);
+        const start = searchParams.get("start");
+        const end = searchParams.get("end");
+    
+        const filter = { payment_status: "completed" };
+    
+        if (start && end) {
+          filter.createdAt = {
+            $gte: new Date(start),
+            $lte: new Date(end),
+          };
+        }
+
+
          
-        const orders = await Order.find({payment_status: "completed"}).sort({_id: -1});
+        const orders = await Order.find(filter).sort({_id: -1}).lean();
         return new Response( JSON.stringify({ message: "Orders Fetched", orders, }), { status: 200 } );
     } catch (error) {
         console.error(error);
--- components/Admin/Orders/Orders.jsx
+++ components/Admin/Orders/Orders.jsx
@@ -1,84 +1,135 @@
 "use client";
 
-import { Space, Table } from "antd";
+import { Space, Table, DatePicker, Button, Menu, Dropdown } from "antd";
+import { AiOutlineEye, AiOutlineCloseCircle } from "react-icons/ai";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import dayjs from "dayjs";
-import { AiOutlineEye } from "react-icons/ai";
+
 import axios from "axios";
 import { OrdersContext } from "@/app/(admin)/admin/layout";
+import quarterOfYear from "dayjs/plugin/quarterOfYear";
+dayjs.extend(quarterOfYear);
 
+const { RangePicker } = DatePicker;
 
-
-export default function Orders( ) {
-
-	const [isLoading, setIsLoading] = useState(false)
+export default function Orders() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [activeLabel, setActiveLabel] = useState("Select Filter");
 
 	const [orders, setOrders] = useState([]);
 
-	useEffect(() => {
+	const [dateRange, setDateRange] = useState(null);
 
-		 
+	const getQuarterRange = () => {
+		const now = dayjs();
+		return [now.startOf("quarter"), now.endOf("quarter")];
+	};
+
+	const getLastQuarterRange = () => {
+		const now = dayjs().subtract(1, "quarter");
+		return [now.startOf("quarter"), now.endOf("quarter")];
+	};
+
+	const monthlyFilters = {
+		"This Month": [dayjs().startOf("month"), dayjs().endOf("month")],
+		"Last Month": [
+			dayjs().subtract(1, "month").startOf("month"),
+			dayjs().subtract(1, "month").endOf("month"),
+		],
+	};
+
+	const quarterlyFilters = {
+		"This Quarter": getQuarterRange(),
+		"Last Quarter": getLastQuarterRange(),
+	};
+
+	const yearlyFilters = {
+		"This Year": [dayjs().startOf("year"), dayjs().endOf("year")],
+		"Last Year": [
+			dayjs().subtract(1, "year").startOf("year"),
+			dayjs().subtract(1, "year").endOf("year"),
+		],
+		"This Financial Year": [
+			dayjs().subtract(1, "year").startOf("month"),
+			dayjs().endOf("month"),
+		],
+		"Last Financial Year": [
+			dayjs().subtract(2, "year").startOf("month"),
+			dayjs().subtract(1, "year").endOf("month"),
+		],
+	};
+
+	useEffect(() => {
 		const getOrders = (async () => {
-			try {
-				setIsLoading(true)
-				const {data, status} = await axios.get('/api/orders/get');
-				console.log(data)
-				if(status === 200) {
-					setOrders(data.orders)
-				}
-			} catch (error) {
-				console.log(error)
-			} finally {
-				setIsLoading(false)
+			const params = {};
+
+			if (dateRange) {
+				const [startDate, endDate] = dateRange;
+				params.start = startDate.toISOString();
+				params.end = endDate.toISOString();
 			}
 
+			try {
+				setIsLoading(true);
+				const { data, status } = await axios.get("/api/orders/get", {
+					params: params,
+				});
+				console.log(data);
+				if (status === 200) {
+					setOrders(data.orders);
+				}
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setIsLoading(false);
+			}
+		})();
+	}, [dateRange]);
 
-		})()
-		
-
-
-
-
-	}, [])
-
-	const {unread_count, set_unread_count} = useContext(OrdersContext)
+	const { unread_count, set_unread_count } = useContext(OrdersContext);
 	const update_order_handler = async (id) => {
-		
-		
 		try {
-			setIsLoading(true)
-			const { data, status} = await axios.post(`/api/orders/update-isread?orderId=${id}`);
+			setIsLoading(true);
+			const { data, status } = await axios.post(
+				`/api/orders/update-isread?orderId=${id}`
+			);
 
-			if(status === 201) {
+			if (status === 201) {
 				setOrders((prev) => {
+					const state = [...prev];
 
-					const state = [...prev]
-
-					const updated_order = state.findIndex(el => el._id === id);
+					const updated_order = state.findIndex(
+						(el) => el._id === id
+					);
 
 					state[updated_order].isRead = true;
 
-					
-					return state
-				})
+					return state;
+				});
 
-				set_unread_count(prev => prev - 1)
+				set_unread_count((prev) => prev - 1);
 			}
 		} catch (error) {
-			console.log(error)
+			console.log(error);
 		} finally {
-			setIsLoading(false)
+			setIsLoading(false);
 		}
-
-	}
+	};
 
 	const columns = [
 		{
 			title: "Order Number",
 			dataIndex: "orderNumber",
 			key: "orderNumber",
-			render: (text, record) => ( <Link href={`/admin/orders/${record._id}`} onClick={() => update_order_handler(record._id)} >{text}</Link> ),
+			render: (text, record) => (
+				<Link
+					href={`/admin/orders/${record._id}`}
+					onClick={() => update_order_handler(record._id)}
+				>
+					{text}
+				</Link>
+			),
 		},
 		{
 			title: "Payment Status",
@@ -90,7 +141,12 @@ export default function Orders( ) {
 			title: "Total Price",
 			dataIndex: "totalPrice",
 			key: "totalPrice",
-			render: (text) => ( <p className="  font-semibold text-base   text-black px-2 py-1 rounded-md  "> £{text} </p> ),
+			render: (text) => (
+				<p className="  font-semibold text-base   text-black px-2 py-1 rounded-md  ">
+					{" "}
+					£{text}{" "}
+				</p>
+			),
 		},
 
 		{
@@ -111,33 +167,129 @@ export default function Orders( ) {
 			title: "Ordered At",
 			dataIndex: "createdAt",
 			key: "createdAt",
-			render: (text) => ( <p className="font-poppins  bg-orange-100 text-orange-800 px-2 py-1 rounded-md  "> {dayjs(text).format("DD-MMM-YYYY  hh:mm A")} </p> ),
+			render: (text) => (
+				<p className="font-poppins  bg-orange-100 text-orange-800 px-2 py-1 rounded-md  ">
+					{" "}
+					{dayjs(text).format("DD-MMM-YYYY  hh:mm A")}{" "}
+				</p>
+			),
 		},
 
 		{
-				  title: 'Action',
-				  key: 'action',
-				  render: (_, record) => {
-						console.log(record)
-					return <Space size="middle"> <button className="flex gap-2 items-center text-sky-500 " onClick={() => update_order_handler(record._id)}><AiOutlineEye className='text-sky-500 scale-150 active:scale-125 hover:scale-[1.7] transition-all' /> Mark as Read</button> </Space>
-				  },
-				},
+			title: "Action",
+			key: "action",
+			render: (_, record) => {
+				console.log(record);
+				return (
+					<Space size="middle">
+						{" "}
+						<button
+							className="flex gap-2 items-center text-sky-500 "
+							onClick={() => update_order_handler(record._id)}
+						>
+							<AiOutlineEye className="text-sky-500 scale-150 active:scale-125 hover:scale-[1.7] transition-all" />{" "}
+							Mark as Read
+						</button>{" "}
+					</Space>
+				);
+			},
+		},
 	];
 
- 
+	const rowClassName = (record) => {
+		// You can apply a different class based on a condition, for example, based on age
+		if (!record.isRead) {
+			return "bg-orange-50 text-orange-800"; // This will apply the 'highlight-row' class to rows where the age is greater than 30
+		}
+		return "";
+	};
 
-		const rowClassName = (record) => {
-		  // You can apply a different class based on a condition, for example, based on age
-		  if (!record.isRead) {
-			return 'bg-orange-50 text-orange-800'; // This will apply the 'highlight-row' class to rows where the age is greater than 30
-		  }
-		  return '';
-		};
+	// const rangeMenu = (
+	// 	<Menu
+	// 		onClick={({ key }) => {
+	// 			setActiveLabel(key)
+	// 			setDateRange(predefinedRanges[key]);
+	// 		}}
+	// 		items={Object.keys(predefinedRanges).map((label) => ({
+	// 			key: label,
+	// 			label,
+	// 		}))}
+	// 	/>
+	// );
 
+	const rangeMenu = (
+		<Menu
+			onClick={({ key }) => {
+				// Flatten all filters
+				const allFilters = {
+					...monthlyFilters,
+					...quarterlyFilters,
+					...yearlyFilters,
+				};
+				setDateRange(allFilters[key]);
+				setActiveLabel(key);
+			}}
+		>
+			<Menu.ItemGroup title="📆 Monthly Filters">
+				{Object.keys(monthlyFilters).map((label) => (
+					<Menu.Item key={label}>{label}</Menu.Item>
+				))}
+			</Menu.ItemGroup>
+
+			<Menu.ItemGroup title="📉 Quarterly Filters">
+				{Object.keys(quarterlyFilters).map((label) => (
+					<Menu.Item key={label}>{label}</Menu.Item>
+				))}
+			</Menu.ItemGroup>
+
+			<Menu.ItemGroup title="📅 Yearly Filters">
+				{Object.keys(yearlyFilters).map((label) => (
+					<Menu.Item key={label}>{label}</Menu.Item>
+				))}
+			</Menu.ItemGroup>
+		</Menu>
+	);
 
 	return (
 		<>
-			<Table columns={columns} dataSource={orders} size="large" showHeader bordered  pagination rowClassName={rowClassName} loading={isLoading}/>
+			<div className="flex flex-wrap gap-2 mb-4 items-center">
+				<RangePicker
+					value={dateRange}
+					onChange={(values) => {
+						if (values) setDateRange(values);
+					}}
+					format="DD-MM-YYYY"
+					allowClear={false}
+					className="ml-2"
+				/>
+				<Dropdown overlay={rangeMenu} placement="bottomLeft" arrow>
+					<Button>{activeLabel}</Button>
+				</Dropdown>
+
+				<Button
+					danger
+					type="text"
+					icon={<AiOutlineCloseCircle />}
+					onClick={() => {
+						setDateRange(null);
+						setActiveLabel("Select Filter");
+					}}
+					className="ml-2"
+				>
+					Clear Filters
+				</Button>
+			</div>
+
+			<Table
+				columns={columns}
+				dataSource={orders}
+				size="large"
+				showHeader
+				bordered
+				pagination
+				rowClassName={rowClassName}
+				loading={isLoading}
+			/>
 		</>
 	);
 }
